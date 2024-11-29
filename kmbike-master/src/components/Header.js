6b115160b9aedd413eba2920f4c1e51120ac5fb6
pannzzz <maria.jimenez544@soy.sena.edu.co>
2024-11-28 22:23:30 -0500

    cambios estilos

--- kmbike-master/src/components/Header.js
+++ kmbike-master/src/components/Header.js
@@ -1,7 +1,7 @@
     import React, { useState } from 'react';
     import { Link } from 'react-router-dom';
     import '../styles/Header.css';
-    import logo from '../assets/Logo.png';  
+    import logo from '../assets/Logo1.png';  
 
     const Header = () => {
     const [menuActive, setMenuActive] = useState(false);
